617141a607bc91e9cef910fdd863b9e23c23b627
jinwood <jinw@protonmail.com>
2019-08-07 13:51:54 +0100

    Add route to get vehicle by user

--- src/controllers/vehicleController.ts
+++ src/controllers/vehicleController.ts
@@ -1,79 +1,110 @@
-import { BaseContext } from 'koa';
-import { getManager, Repository, Not, Equal } from 'typeorm';
-import { validate, ValidationError } from 'class-validator';
-import { Vehicle } from 'models/vehicle';
+import { BaseContext } from "koa";
+import { getManager, Repository, getConnection } from "typeorm";
+import { validate, ValidationError } from "class-validator";
+import { Vehicle } from "models/vehicle";
+import { User } from "models/user";
 
-export default class VehicleController { 
+export default class VehicleController {
+  public static async getVehicles(ctx: BaseContext) {
+    const vehicleRepository: Repository<Vehicle> = getManager().getRepository(
+      Vehicle
+    );
 
-    public static async getVehicles (ctx: BaseContext) {
+    const vehicles: Vehicle[] = await vehicleRepository.find();
 
-        const vehicleRepository: Repository<Vehicle> = getManager().getRepository(Vehicle);
+    ctx.status = 200;
+    ctx.body = vehicles;
+  }
 
-        const vehicles: Vehicle[] = await vehicleRepository.find();
+  public static async getVehicle(ctx: BaseContext) {
+    const vehicleRepository: Repository<Vehicle> = getManager().getRepository(
+      Vehicle
+    );
 
-        ctx.status = 200;
-        ctx.body = vehicles;
+    const vehicle: Vehicle = await vehicleRepository.findOne(ctx.params.id);
+
+    if (vehicle) {
+      ctx.status = 200;
+      ctx.body = vehicle;
+    } else {
+      ctx.status = 400;
+      ctx.body = `couldn't find vehicle with id ${ctx.params.id}`;
     }
+  }
 
-    public static async getVehicle (ctx: BaseContext) {
+  public static async getVehicleByUserId(ctx: BaseContext) {
+    const vehicleRepository: Repository<Vehicle> = getManager().getRepository(
+      Vehicle
+    );
 
-        const vehicleRepository: Repository<Vehicle> = getManager().getRepository(Vehicle);
+    const vehicle: Vehicle = await vehicleRepository
+      .createQueryBuilder("vehicle")
+      .leftJoin("vehicle.user", "user", "user.id = :id", {
+        id: ctx.params.id
+      })
+      .getOne();
 
-        const vehicle: Vehicle = await vehicleRepository.findOne(ctx.params.id);
-
-        if (vehicle) {
-            ctx.status = 200;
-            ctx.body = vehicle;
-        } else {
-            ctx.status = 400;
-            ctx.body = `couldn't find vehicle with id ${ctx.params.id}`;
-        }
+    if (vehicle) {
+      ctx.status = 200;
+      ctx.body = vehicle;
+    } else {
+      ctx.status = 400;
+      ctx.body = `no vehicles associated with user ${ctx.params.id}`;
     }
+  }
 
-    public static async getVehicleByRegistration (ctx: BaseContext) {
+  public static async getVehicleByRegistration(ctx: BaseContext) {
+    console.log("by reg");
 
-        console.log('by reg'); 
+    const vehicleRepository: Repository<Vehicle> = getManager().getRepository(
+      Vehicle
+    );
 
-        const vehicleRepository: Repository<Vehicle> = getManager().getRepository(Vehicle);
+    const vehicle: Vehicle = await vehicleRepository.findOne({
+      where: {
+        registration: ctx.params.registration
+      }
+    });
 
-        const vehicle: Vehicle = await vehicleRepository.findOne({
-            where: {
-                registration: ctx.params.registration
-            }
-        });
-
-        if (vehicle) {
-            ctx.status = 200;
-            ctx.body = vehicle;
-        } else {
-            ctx.status = 400;
-            ctx.body = `couldn't find vehicle with reg ${ctx.params.registration}`;
-        }
+    if (vehicle) {
+      ctx.status = 200;
+      ctx.body = vehicle;
+    } else {
+      ctx.status = 400;
+      ctx.body = `couldn't find vehicle with reg ${ctx.params.registration}`;
     }
+  }
 
-    public static async createVehicle (ctx: BaseContext) { 
+  public static async createVehicle(ctx: BaseContext) {
+    const vehicleRepository: Repository<Vehicle> = getManager().getRepository(
+      Vehicle
+    );
 
-        const vehicleRepository: Repository<Vehicle> = getManager().getRepository(Vehicle);
+    const newVehicle: Vehicle = new Vehicle();
 
-        const newVehicle: Vehicle = new Vehicle();
+    newVehicle.manufacturer = ctx.request.body.manufacturer;
+    newVehicle.model = ctx.request.body.model;
+    newVehicle.engineSize = ctx.request.body.engineSize;
+    newVehicle.registration = ctx.request.body.registration;
+    newVehicle.user = ctx.request.body.user;
 
-        newVehicle.manufacturer = ctx.request.body.manufacturer;
-        newVehicle.model = ctx.request.body.model;
-        newVehicle.engineSize = ctx.request.body.engineSize;
-        newVehicle.registration = ctx.request.body.registration;
-        newVehicle.user = ctx.request.body.user;
-
-        const validationErrors: ValidationError[] = await validate(newVehicle, { skipMissingProperties: true });
-        if (validationErrors.length > 0) {
-            ctx.status = 400;
-            ctx.body = validationErrors;
-        } else if (await vehicleRepository.findOne({ registration: newVehicle.registration })) {
-            ctx.status = 400;
-            ctx.body =  `the vehicle with registration ${newVehicle.registration} already exists`;
-        } else {
-            const vehicle = vehicleRepository.save(newVehicle);
-            ctx.status = 201;
-            ctx.body = vehicle;
-        }
+    const validationErrors: ValidationError[] = await validate(newVehicle, {
+      skipMissingProperties: true
+    });
+    if (validationErrors.length > 0) {
+      ctx.status = 400;
+      ctx.body = validationErrors;
+    } else if (
+      await vehicleRepository.findOne({ registration: newVehicle.registration })
+    ) {
+      ctx.status = 400;
+      ctx.body = `the vehicle with registration ${
+        newVehicle.registration
+      } already exists`;
+    } else {
+      const vehicle = vehicleRepository.save(newVehicle);
+      ctx.status = 201;
+      ctx.body = vehicle;
     }
+  }
 }
--- src/routes/crud-routes.ts
+++ src/routes/crud-routes.ts
@@ -1,23 +1,28 @@
-import Router from 'koa-router';
-import userController from 'controllers/userController';
-import vehicleController from 'controllers/vehicleController';
-import HistoryItemController from 'controllers/historyItemController';
+import Router from "koa-router";
+import userController from "controllers/userController";
+import vehicleController from "controllers/vehicleController";
+import HistoryItemController from "controllers/historyItemController";
 
 export const crudRouter: Router = new Router();
 
 //user
-crudRouter.get('/users', userController.getUsers);
-crudRouter.get('/users/:id', userController.getUser);
-crudRouter.post('/users', userController.createUser);
-crudRouter.put('/users/:id', userController.updateUser);
-crudRouter.delete('/users/:id', userController.deleteUser);
+crudRouter.get("/users", userController.getUsers);
+crudRouter.get("/users/:id", userController.getUser);
+crudRouter.post("/users", userController.createUser);
+crudRouter.put("/users/:id", userController.updateUser);
+crudRouter.delete("/users/:id", userController.deleteUser);
+crudRouter.get("/users/:id/vehicles", userController.getUserVehicles);
 
 //vehicle
-crudRouter.get('/vehicles', vehicleController.getVehicles);
-crudRouter.get('/vehicles/:id', vehicleController.getVehicle);
-crudRouter.post('/vehicles', vehicleController.createVehicle);
+crudRouter.get("/vehicles", vehicleController.getVehicles);
+crudRouter.get("/vehicles/byUserId/:id", vehicleController.getVehicleByUserId);
+crudRouter.get("/vehicles/:id", vehicleController.getVehicle);
+crudRouter.post("/vehicles", vehicleController.createVehicle);
 
-crudRouter.get('/vehicles/byregistration/:registration', vehicleController.getVehicleByRegistration);
+crudRouter.get(
+  "/vehicles/byregistration/:registration",
+  vehicleController.getVehicleByRegistration
+);
 
 //history items
-crudRouter.get('/historyitems', HistoryItemController.getHistoryItems);
+crudRouter.get("/historyitems", HistoryItemController.getHistoryItems);
