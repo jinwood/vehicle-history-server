e3993236f9a0181e24387d785dceef6c21f70a6c
jinwood <jinw@protonmail.com>
2019-08-18 21:39:14 +0100

    Rough history save endpoint

--- src/controllers/historyItemController.ts
+++ src/controllers/historyItemController.ts
@@ -1,16 +1,51 @@
-import { BaseContext } from "koa";
-import { Repository, getManager } from "typeorm";
-import { HistoryItem } from "models/historyItem";
+import { BaseContext } from 'koa';
+import { Repository, getManager } from 'typeorm';
+import { HistoryItem } from 'models/historyItem';
+import { ValidationError, validate } from 'class-validator';
 
 export default class HistoryItemController {
+  public static async getHistoryItems(ctx: BaseContext) {
+    const historyItemRepository: Repository<
+      HistoryItem
+    > = getManager().getRepository(HistoryItem);
 
-    public static async getHistoryItems (ctx: BaseContext) {
+    const historyItems: HistoryItem[] = await historyItemRepository.find();
 
-        const historyItemRepository: Repository<HistoryItem> = getManager().getRepository(HistoryItem);
+    ctx.status = 200;
+    ctx.body = historyItems;
+  }
 
-        const historyItems: HistoryItem[] = await historyItemRepository.find();
+  public static async createHistoryItem(ctx: BaseContext) {
+    const historyItemRepository: Repository<
+      HistoryItem
+    > = getManager().getRepository(HistoryItem);
 
-        ctx.status = 200;
-        ctx.body = historyItems;
+    const newHistoryItem: HistoryItem = new HistoryItem();
+    const { historyItemType, description } = ctx.request.body.historyItem;
+
+    newHistoryItem.type = historyItemType;
+    newHistoryItem.description = description;
+
+    const validationErrors: ValidationError[] = await validate(newHistoryItem, {
+      skipMissingProperties: true
+    });
+
+    if (validationErrors.length > 0) {
+      ctx.status = 400;
+      ctx.response.body = { validationErrors };
+    } else {
+      try {
+        const historyItem = historyItemRepository
+          .save(newHistoryItem)
+          .then(h => {
+            console.log(`created vehicle with id ${h.id}`);
+          });
+        ctx.status = 201;
+        ctx.response.body = { historyItem };
+      } catch (err) {
+        ctx.status = 500;
+        ctx.response.body = { err };
+      }
     }
+  }
 }
