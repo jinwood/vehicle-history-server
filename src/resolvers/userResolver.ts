a6f469d42f97a265d14a444d5130ceee036e361a
jinwood <jinw@protonmail.com>
2019-11-24 19:21:05 +0000

    Relational get user vehicle working

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -3,16 +3,25 @@ import {
   Resolver,
   Query,
   Arg,
-  Mutation} from 'type-graphql';
+  Mutation,
+  FieldResolver,
+  Root,
+  ResolverInterface
+} from 'type-graphql';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { Repository, DeleteResult } from 'typeorm';
 import { User } from '../entities/user';
 import { UserInput } from './types/user-input';
 import { ValidationError } from 'apollo-server';
+import { Vehicle } from '../entities/vehicle';
 
-@Resolver(() => User)
-export class UserResolver {
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>){}
+@Resolver(of => User)
+export class UserResolver implements ResolverInterface<User> {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Vehicle)
+    private readonly vehicleRepository: Repository<Vehicle>
+  ) {}
 
   //get 1
   @Query(() => User)
@@ -22,16 +31,14 @@ export class UserResolver {
 
   //get all
   @Query(() => [User])
-  users()  {
+  users() {
     const users = this.userRepository.find();
     return users;
   }
 
   //add 1
   @Mutation(returns => User)
-  addUser(
-    @Arg('user') userInput: UserInput
-  ): Promise<User> {
+  addUser(@Arg('user') userInput: UserInput): Promise<User> {
     const user = this.userRepository.create({
       ...userInput
     });
@@ -43,11 +50,21 @@ export class UserResolver {
   async deleteUser(@Arg('userId') userId: string): Promise<string> {
     const deletee = await this.userRepository.findOne(userId);
 
-    if(deletee) {
+    if (deletee) {
       const deleted = this.userRepository.remove(deletee);
       return deletee.id;
     } else {
-      return 'no entity found'
+      return 'no entity found';
     }
+  }
+
+  @FieldResolver()
+  async vehicles(@Root() user: User) {
+    const vehicle = await this.vehicleRepository.findOne({
+      where: { userId: user.id }
+    });
+    console.table(vehicle);
+    if (!vehicle) throw new Error();
+    return [vehicle];
   }
 }
