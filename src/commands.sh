ebc79daebaa576f2f55d7402a918bd6c5b60dc5a
jinwood <jinw@protonmail.com>
2020-08-27 12:08:28 +0100

    This is it

--- src/commands.sh
+++ src/commands.sh
@@ -1,3 +1,5 @@
 #Create user
-
 http POST http://localhost:4000/users name="Julian" familyName="Inwood" email="foo@bar.com" hashedPassword="password"
+
+#Get uers
+http GET http://localhost:4000/users
